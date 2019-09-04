48a0f836cae63b1f151c30a4d76aea06e10091f9
Raphael Lautenbaer <raphael.lautenbacher@googlemail.com>
2019-09-04 17:38:51 +0200

    backend v1

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const bcrypt = require('bcrypt-nodejs');
+const hash = bcrypt.hashSync('bacon');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'datingel',
+    password: '',
+    database: 'smart-brain',
+  },
+});
+
+db.select('*')
+  .from('users')
+  .then(data => {});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post('/register', register.handleRegister(db, bcrypt)); //other syntax to write the endpoint-> see register.js
+
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.hanldeImagePut(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+  console.log(`Server is listening on ${port}`);
+});
